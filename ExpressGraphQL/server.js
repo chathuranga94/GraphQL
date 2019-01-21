543fe9a7075ee8ef4de3e619928c12c731957432
Udara Bibile <chathuranga94@live.com>
2019-01-21 23:44:53 +0530

    express grahql

---
+++ ExpressGraphQL/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+  }
+`);
+
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+let fakeDatabase = {};
+
+const root = {
+  getMessage:  ({id}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    const id = require('crypto').randomBytes(10).toString('hex');
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
